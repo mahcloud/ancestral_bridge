621ad16e809ed6311bc4c2d9e245f1c05c887fd0
Michael Hoffert <mike.hoffert@gmail.com>
2013-02-07 21:49:18 -0600

    initial rails app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AncestralBridge::Application.config.secret_token = 'c943dbff1f7c90cf4972b0a49f5b1bb53f0c792bd2c539ec33311c1e1220960fadd4f732a6a9b012f1ab5d458413ed4dae3ef9ff585dc8a9898e1740afb29b75'
